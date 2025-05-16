345c58a0aedb12a3ad87bf7e57db7a524dc742e9
Christian Frank <56785917+ccnfrank@users.noreply.github.com>
2025-05-16 18:51:21 +0200

    coor

--- main.py
+++ main.py
@@ -2,9 +2,5 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from auth_routes import auth_router  # noqa: E402
-from order_routes import order_router  # noqa: E402
-
+from auth_routes import auth_router
 app.include_router(auth_router)
-app.include_router(order_router)
-
