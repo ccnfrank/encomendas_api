7dc44881c5d29b9bc8da466d7c822c39656daaaa
Christian Frank <56785917+ccnfrank@users.noreply.github.com>
2025-05-16 17:20:55 +0200

    iniciando contructor and documentation

--- auth_routes.py
+++ auth_routes.py
@@ -2,3 +2,11 @@ from fastapi import APIRouter
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
+@auth_router.get("/")
+async def autent():
+    """
+    api criada publica por christian frank. todos user precisam de autenticacao \n
+    caso voce ainda nao tenha recebido seu login / pass, entre em contato com ccnfrank@gmail.com
+    
+    """
+    return {"msg":"rota autenticacao","autenticado":False}
--- order_routes.py
+++ order_routes.py
@@ -2,3 +2,11 @@ from fastapi import APIRouter
 order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
 
 
+@order_router.get("/")
+async def funcao():
+    return {"first_msg":"iniciar construcao api"}
+    
+
+
+
+
