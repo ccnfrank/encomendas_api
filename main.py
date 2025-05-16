42cbbf221fe9de7b39cc08f7a5c12f82b933bee3
Christian Frank <56785917+ccnfrank@users.noreply.github.com>
2025-05-16 16:52:19 +0200

    add api_router

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from auth_routes import auth_router
-from order_routes import order_router
+from auth_routes import auth_router  # noqa: E402
+from order_routes import order_router  # noqa: E402
 
 app.include_router(auth_router)
 app.include_router(order_router)
